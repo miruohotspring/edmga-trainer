138a5089db9d17401eca488ffa2b1a2ae721a375
miruohotspring <miruohotspring@gmail.com>
2021-09-15 10:29:44 +0900

    reverting commit

--- edmga_trainer/utils.py
+++ edmga_trainer/utils.py
@@ -7,46 +7,24 @@ import time
 from . import credentials
 
 class DataLoader():
-    def __init__(self, add_new=False, start=0, end=0):
-        if add_new == True:
-            if start == 0 or start > end:
-                print("Check out of start and end number.")
-                exit()
-            else:
-                add_new_data(start, end)
-        (self.data, self.target) = load_data()
+    def __init__(self, n=1):
+        create_isrc_files(n)
         return
 
-# 新しく各ジャンルのstartページ目からendページ目までの曲データをファイルに保存
-def add_new_data(start, end):
-    create_isrc_files(start, end)
-    download_features()
-
-# ファイルから教師データをロードします
-def load_data():
-    data = []
-    target = []
-    for i, genre in enumerate(genres_list):
-        data_genre = np.loadtxt(f"./data/{genre}.txt", delimiter='\n', dtype='str')
-        for d in data_genre:
-            d.replace("[", "").replace("]", "")
-            data.append(np.fromstring(d, dtype=float, sep=', ').tolist())
-            target.append(i)
-    return (np.array(data), np.array(target))
-
-# 各ジャンルにつきn*100曲のISRCコードをファイルに書き出す
-def create_isrc_files(start, end):
+# 各ジャンルにつきn*100曲のISRCコードをファイルに書き出します
+def create_isrc_files(n):
+    
     r = requests.get("https://www.beatport.com/api/v4/catalog/genres")
     lists = r.json()["results"]
     
-    for i, genre in enumerate(lists):
+    for i, genre in enumerate(lists[23:]):
         print("loading " + genre["name"] + f"...[{i+1}/{len(lists)}]")
         track_ids = []
         isrc = []
         slug = genre["slug"]
         id = genre["id"]
         path = f"./isrc/{slug}.txt"
-        for batch in range(start, end):
+        for batch in range(n):
             uri = f"https://www.beatport.com/genre/{slug}/{id}/tracks?page={batch+1}"
             r = requests.get(uri)
             track_ids.extend(re.findall(r'data-track="([0-9]+)', r.text))
